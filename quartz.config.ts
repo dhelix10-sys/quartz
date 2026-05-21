81ec9c60d87cdb5a26c6a4ce51e9c8263379afc0
dhelix10-sys <dhelix10@gmail.com>
2026-05-21 09:27:47 +0900

    첫 번째 쿼츠 블로그 설정

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "OK쌤 블로그",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
